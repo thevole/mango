470fe5641e9b0ae78500be4b052c5292ead6cf3a
Martin Volerich <martin@volerich.com>
2017-08-21 22:22:28 -0700

    Registration

--- lib/mango_web/router.ex
+++ lib/mango_web/router.ex
@@ -18,6 +18,9 @@ defmodule MangoWeb.Router do
 
     get "/", PageController, :index
     get "/categories/:name", CategoryController, :show
+
+    get "/register", RegistrationController, :new
+    post "/register", RegistrationController, :create
   end
 
   # Other scopes may use custom stacks.
