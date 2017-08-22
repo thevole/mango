2692977d6796108f0a8ff11d3438b39a02bfd975
Martin Volerich <martin@volerich.com>
2017-08-21 17:44:24 -0700

    Working catalog view

---
+++ lib/mango/catalog/catalog.ex
@@ -0,0 +1,20 @@
+defmodule Mango.Catalog do
+  alias Mango.Catalog.Product
+  alias Mango.Repo
+
+  def list_products do
+    Product
+    |> Repo.all
+  end
+
+  def list_seasonal_products do
+    list_products()
+    |> Enum.filter(&(&1.is_seasonal == true))
+  end
+
+  def get_category_products(name) do
+    list_products()
+    |> Enum.filter(&(&1.category == name))
+  end
+
+end
--- test/mango_web/acceptance/homepage_test.exs
+++ test/mango_web/acceptance/homepage_test.exs
@@ -1,12 +1,42 @@
-defmodule MangoWeb.HomepageTest do
-  use ExUnit.Case
+defmodule MangoWeb.Acceptance.HomepageTest do
+  use Mango.DataCase
   use Hound.Helpers
 
   hound_session()
 
-  test "presence of featured products" do
+  setup do
+    alias Mango.Repo
+    alias Mango.Catalog.Product
+    Repo.insert %Product{name: "Tomato", price: 50, is_seasonal: false, category: "vegetables"}
+    Repo.insert %Product{name: "Apple", price: 100, is_seasonal: true, category: "fruits"}
+    :ok
+  end
+
+  test "presence of seasonal products" do
+    ## GIVEN ##
+    # There are two products Apple and Tomato price 100 and 50 respectively
+    # Where Apple being the only seasonal product
+
+    ## WHEN ##
+    # I navigate to homepage
     navigate_to("/")
 
-    assert page_source() =~ "Seasonal products"
+    ## THEN ##
+    # I expect the page title to be "Seasonal products"
+    page_title = find_element(:css, ".page-title") |> visible_text()
+    assert page_title == "Seasonal products"
+
+    # And I expect Apple in the product displayed
+    product = find_element(:css, ".product")
+    product_name = find_within_element(product, :css, ".product-name") |> visible_text()
+    product_price = find_within_element(product, :css, ".product-price") |> visible_text()
+
+    assert product_name == "Apple"
+    # And I expect its price to be displayed on screen
+    assert product_price == "100"
+
+    # And I expect that Tomato is not present on screen.
+    refute page_source() =~ "Tomato"
   end
+
 end
