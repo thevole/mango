164873d752985985b4828d318234e0b026ea46bf
Martin Volerich <martin@volerich.com>
2017-09-03 11:30:48 -0700

    Butchered ticket handling - no exception or UI ingres

--- lib/mango/crm/crm.ex
+++ lib/mango/crm/crm.ex
@@ -34,4 +34,73 @@ defmodule Mango.CRM do
     |> Ecto.assoc(:orders)
     |> Repo.all
   end
+
+  alias Mango.CRM.Ticket
+
+  @doc """
+  Returns the list of tickets.
+
+  ## Examples
+
+      iex> list_customer_tickets(customer)
+      [%Ticket{}, ...]
+
+  """
+  def list_customer_tickets(customer = %Customer{}) do
+    customer
+    |> Ecto.assoc(:tickets)
+    |> Repo.all()
+  end
+
+  @doc """
+  Gets a single ticket.
+
+  Raises `Ecto.NoResultsError` if the Ticket does not exist.
+
+  ## Examples
+
+      iex> get_customer_ticket!(customer, 123)
+      %Ticket{}
+
+      iex> get_customer_ticket!(456)
+      ** (Ecto.NoResultsError)
+
+  """
+  def get_customer_ticket!(customer = %Customer{}, id) do
+    customer
+    |> Ecto.assoc(:tickets)
+    |> Repo.get!(id)
+  end
+
+  @doc """
+  Creates a ticket.
+
+  ## Examples
+
+      iex> create_customer_ticket(customer, %{field: value})
+      {:ok, %Ticket{}}
+
+      iex> create_customer_ticket(customer, %{field: bad_value})
+      {:error, %Ecto.Changeset{}}
+
+  """
+  def create_customer_ticket(customer = %Customer{}, attrs \\ %{}) do
+    build_customer_ticket(customer, attrs)
+    |> Repo.insert()
+  end
+
+
+  @doc """
+  Returns an `%Ecto.Changeset{}` for tracking ticket changes.
+
+  ## Examples
+
+      iex> build_customer_ticket(customer, %{})
+      %Ecto.Changeset{source: %Ticket{}}
+
+  """
+  def build_customer_ticket(customer = %Customer{}, attrs \\ %{}) do
+    Ecto.build_assoc(customer, :tickets, %{status: "New"})
+    |> Ticket.changeset(attrs)
+  end
 end
--- lib/mango_web/router.ex
+++ lib/mango_web/router.ex
@@ -47,5 +47,7 @@ defmodule MangoWeb.Router do
     get "/logout", SessionController, :delete
     get "/checkout", CheckoutController, :edit
     put "/checkout/confirm", CheckoutController, :update
+
+    resources "/tickets", TicketController, except: [:edit, :update, :delete]
   end
 end
